c7f37dc7fa89625f393164c7cba29d49234d31dc
justinp24 <justinpardo2423@gmail.com>
2024-06-27 12:23:07 -0400

    enable searchbar to make api calls using useffect

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -5,6 +5,10 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function App() {
+    // so i will be making an api conneciton with rawg
+    // and it will be used as a search bar when searching
+    // for games and a small display will appear under the search
+    // bar with whatever rawg api finds matches the inpout field
     useEffect(() => {});
     return (
         <div className="app">
--- my-first-react-app/src/pages/Homepage.jsx
+++ my-first-react-app/src/pages/Homepage.jsx
@@ -5,10 +5,35 @@ import fortnite from "../assets/fortnite.svg";
 import steam from "../assets/steam.svg";
 import act from "../assets/act.svg";
 import "../App.css";
-
+// so i will be making an api conneciton with rawg
+// and it will be used as a search bar when searching
+// for games and a small display will appear under the search
+// bar with whatever rawg api finds matches the inpout field
+//password mimitigremimi
+// key 59fc59e388264249be02c76b5db71a6
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Homepage() {
+    const [value, setValue] = useState("");
+
+    useEffect(() => {
+        if (value) {
+            const apikey = "59fc59e388264249be02c76b5db71a62";
+            const url = `https://api.rawg.io/api/games?key=${apikey}&search=${value}`;
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => console.log(data))
+                .catch((error) => console.error(error));
+        }
+    }, [value]);
+
+    const handleInput = (e) => {
+        const vt = typeof e.target.value;
+        console.log(vt);
+        setValue(e.target.value);
+    };
+
     return (
         <div className="homepage-container">
             <div className="logos">
@@ -35,7 +60,12 @@ export default function Homepage() {
                         voluptatum nam consequuntur blanditiis vero illum
                         officiis.
                     </p>
-                    <input type="`text`" placeholder="Search Games..." />
+                    <input
+                        type="`text`"
+                        value={value}
+                        onChange={handleInput}
+                        placeholder="Search Games..."
+                    />
                     <div className="button-container">
                         <button>Github</button>
                         <Link to="library">
